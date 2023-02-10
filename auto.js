2ef4112c830b32ea0fbe933791df210476b3d72a
yokota4 <yokota@nanomedia.jp>
2023-02-10 09:49:17 +0900

    feat:管理の終了のタイミングを通知するトリガーも設定。

--- auto.js
+++ auto.js
@@ -32,6 +32,26 @@ function notifyAirExchange() {
     }
     message += "換気の時間になりました。\n5~10分換気を行ってください(bow)";
     sendMessageCw( message, roomId_four );
+    ScriptApp.newTrigger( 'notifyAirExchangeEnd' ).timeBased().after( 5 * 60 * 1000 ).create();
+  } catch( e ) {
+    throw log( funcName, e, { type: "error", output: "return", } );
+  }
+}
+
+function notifyAirExchangeEnd() {
+  const funcName = "notifyAirExchange";
+  let message = "";
+  if ( isHoliday( todayObject ) ) {
+    log( funcName, "Today is holiday.", { type: "info", } );
+    return;
+  }
+  try {
+    const members = commuteListSh.getPrevMembers();
+    for ( let i = 0; i < members.length; i++ ) {
+      message += mention( members[ i ].accountId, members[ i ].name ) + "\n";
+    }
+    message += "管理終了です。\nご協力ありがとうございました(bow)";
+    sendMessageCw( message, roomId_four );
   } catch( e ) {
     throw log( funcName, e, { type: "error", output: "return", } );
   }
